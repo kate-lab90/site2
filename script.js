3abf8e1d4d65fc4ee9da057e09fa22c0f9602786
Kate Labunets <kasyanenko_eg@mail.ru>
2022-09-13 18:41:30 +0300

    modified css

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.querySelector('.menu-burger').addEventListener('cklick', function (e) {
-  e.preventDefault();
-  document.querySelector('nav').innerHTML.toggle('is-active');
+  console.log('123')
+  /*document.querySelector('nav').classList.add('is-active');*/
 });
+
